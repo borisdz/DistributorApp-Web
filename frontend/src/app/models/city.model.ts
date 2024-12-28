e2639aaa807a53d85014d79c874849900788858e
Boris Djotov <borisdzotov@gmail.com>
2024-12-28 18:53:14 +0100

    Removed frontend project to fix gitignore

--- frontend/src/app/models/city.model.ts
+++
@@ -1,9 +0,0 @@
-import { UserModel } from './user.model';
-import { WarehouseModel } from './warehouse.model';
-
-export interface CityModel {
-  cityId: number;
-  cityName: string;
-  users?: UserModel[];
-  warehouses?: WarehouseModel[];
-}
