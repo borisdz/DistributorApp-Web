ba4a3de66a9b7098db9306a43d23b5cfcaa7c474
Boris Djotov <borisdzotov@gmail.com>
2025-01-13 17:25:06 +0100

    Created login, register, and role specific homepages.

---
+++ distributor-app-frontend/src/app/components/home/distributor-home/distributor-home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-distributor-home',
+  imports: [],
+  templateUrl: './distributor-home.component.html',
+  styleUrl: './distributor-home.component.css'
+})
+export class DistributorHomeComponent {
+
+}
