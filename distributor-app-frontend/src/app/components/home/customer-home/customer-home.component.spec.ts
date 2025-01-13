7866ed121303b720753731b1683852e6342968cd
Boris Djotov <borisdzotov@gmail.com>
2025-01-13 17:25:06 +0100

    Created login, register, and role specific homepages.

--- distributor-app-frontend/src/app/components/home/customer-home/customer-home.component.spec.ts
+++ distributor-app-frontend/src/app/components/home/customer-home/customer-home.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NavbarComponent } from './navbar.component';
+import { CustomerHomeComponent } from './customer-home.component';
 
-describe('NavbarComponent', () => {
-  let component: NavbarComponent;
-  let fixture: ComponentFixture<NavbarComponent>;
+describe('CustomerHomeComponent', () => {
+  let component: CustomerHomeComponent;
+  let fixture: ComponentFixture<CustomerHomeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NavbarComponent]
+      imports: [CustomerHomeComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(NavbarComponent);
+    fixture = TestBed.createComponent(CustomerHomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
