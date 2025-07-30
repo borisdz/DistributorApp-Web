a1eaac53805f85b911e08c73f35b3adf2a185484
Boris Djotov <borisdzotov@gmail.com>
2025-07-30 19:41:11 +0200

    Left over changes

--- src/main/java/mk/ukim/finki/db/distributorapp/_security/SecurityConfig.java
+++ src/main/java/mk/ukim/finki/db/distributorapp/_security/SecurityConfig.java
@@ -26,9 +26,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Configuration
 @RequiredArgsConstructor
@@ -85,12 +83,12 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        String cleanedOrigins = allowedOrigins.replace("\"", "");
-        List<String> origins = Arrays.stream(cleanedOrigins.split(","))
-                .map(String::trim)
-                .collect(Collectors.toList());
+//        String cleanedOrigins = allowedOrigins.replace("\"", "");
+//        List<String> origins = Arrays.stream(cleanedOrigins.split(","))
+//                .map(String::trim)
+//                .collect(Collectors.toList());
 
-        configuration.setAllowedOriginPatterns(origins);
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
